93460fb3834a315705c5d58a7b74d3c584b84668
Omar shalaby <31737511+omarzero130@users.noreply.github.com>
2024-02-18 01:17:18 +0200

    Separate test data

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,13 +1,10 @@
 package Pages;
-
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.List;
+
 
 public class LoginPage extends PageBase{
     public LoginPage(AppiumDriver driver) {
@@ -18,8 +15,8 @@ public class LoginPage extends PageBase{
     public void LoginToApp () throws InterruptedException{
         //Thread.sleep(10000);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-        LoginFields.get(0).sendKeys("00000111");
-        LoginFields.get(1).sendKeys("4saleTest");
+        LoginFields.get(0).sendKeys(Utils.LoginTestData.USER_PHONE);
+        LoginFields.get(1).sendKeys(Utils.LoginTestData.USER_PASSWORD);
         driver.findElement(LoginButton).click();
     }
 
---
+++ src/main/java/Utils/LoginTestData.java
@@ -0,0 +1,7 @@
+package Utils;
+
+public class LoginTestData {
+
+    public static final String USER_PHONE = "00000111";
+    public static final String USER_PASSWORD = "4saleTest";
+}
