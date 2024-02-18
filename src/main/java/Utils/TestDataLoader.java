202c98baffd2f9df027cea82807806d5bf0076c6
Omar shalaby <31737511+omarzero130@users.noreply.github.com>
2024-02-18 23:17:23 +0200

    Add TestDataLoader and properties file

---
+++ src/main/java/Utils/TestDataLoader.java
@@ -0,0 +1,35 @@
+package Utils;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestDataLoader {
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            // Load properties file
+            properties.load(new FileReader(System.getProperty("user.dir")+"/src/main/resources/Configs/testdata.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getUsername() {
+        return properties.getProperty("username");
+    }
+
+    public static String getPassword() {
+        return properties.getProperty("password");
+    }
+
+    public static String getTitle() {
+        return properties.getProperty("AdTitle");
+    }
+
+    public static String getPrice() {
+        return properties.getProperty("AdPrice");
+    }
+}
--- src/test/java/testCases/Login.java
+++ src/test/java/testCases/Login.java
@@ -4,6 +4,7 @@ import Pages.ChooseLocationPage;
 import Pages.HomePage;
 import Pages.LoginPage;
 import Pages.PostAdPage;
+import Utils.TestDataLoader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,38 +20,47 @@ public class Login extends TestBase{
     boolean isDisplayed;
 
     @Test
-    public void login () throws InterruptedException {
-        homepage = new HomePage(super.appiumDriver);
-        homepage.clickOnPostAddButton();
-        loginpage = new LoginPage(super.appiumDriver);
-        loginpage.LoginToApp();
-        postAdPage =new PostAdPage(super.appiumDriver);
-        postAdPage.ClickOnNextButton();
-        isDisplayed= postAdPage.ValidateErrorPopup();
-        Assert.assertTrue(isDisplayed,"Validation pop up is displayed");
-        postAdPage.ClickOnCategoryField();
-        postAdPage.ChooseMainCategory();
-        postAdPage.ChooseSubCategory();
-        postAdPage.ClickOnDistrictField();
-        postAdPage.ClickOnDistrictLocationField();
-        chooselocation= new ChooseLocationPage(super.appiumDriver);
-        chooselocation.ChooseDistrict();
-        chooselocation.ClickonDone();
-        chooselocation.ClickonAreaField();
-        chooselocation.ChooseArea();
-        chooselocation.ClickonDone();
-        chooselocation.ClickOnBlockField();
-        chooselocation.ChooseBlock();
-        chooselocation.ClickonDone();
-        chooselocation.ClickOnConfirmLocation();
-        postAdPage.EnterTitleField();
-        postAdPage.EnterPriceField();
-        postAdPage.ClickOnNextButton();
+    public void testLogin() {
+        String username = TestDataLoader.getUsername();
+        String password = TestDataLoader.getPassword();
 
+        System.out.println("Credentials is"+username + password);
 
-
-
-
+        // Perform login using username and password
     }
+//    @Test
+//    public void login () throws InterruptedException {
+//        homepage = new HomePage(super.appiumDriver);
+//        homepage.clickOnPostAddButton();
+//        loginpage = new LoginPage(super.appiumDriver);
+//        loginpage.LoginToApp();
+//        postAdPage =new PostAdPage(super.appiumDriver);
+//        postAdPage.ClickOnNextButton();
+//        isDisplayed= postAdPage.ValidateErrorPopup();
+//        Assert.assertTrue(isDisplayed,"Validation pop up is displayed");
+//        postAdPage.ClickOnCategoryField();
+//        postAdPage.ChooseMainCategory();
+//        postAdPage.ChooseSubCategory();
+//        postAdPage.ClickOnDistrictField();
+//        postAdPage.ClickOnDistrictLocationField();
+//        chooselocation= new ChooseLocationPage(super.appiumDriver);
+//        chooselocation.ChooseDistrict();
+//        chooselocation.ClickonDone();
+//        chooselocation.ClickonAreaField();
+//        chooselocation.ChooseArea();
+//        chooselocation.ClickonDone();
+//        chooselocation.ClickOnBlockField();
+//        chooselocation.ChooseBlock();
+//        chooselocation.ClickonDone();
+//        chooselocation.ClickOnConfirmLocation();
+//        postAdPage.EnterTitleField();
+//        postAdPage.EnterPriceField();
+//        postAdPage.ClickOnNextButton();
+//
+//
+//
+//
+//
+//    }
 
 }
