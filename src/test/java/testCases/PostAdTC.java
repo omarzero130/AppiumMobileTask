c45870577b39654507b94a4f1e5e934c4a8fe47d
Omar shalaby <31737511+omarzero130@users.noreply.github.com>
2024-02-20 16:21:19 +0200

    Update testcases and test suite

--- src/test/java/testCases/PostAdTC.java
+++ src/test/java/testCases/PostAdTC.java
@@ -1,4 +1,31 @@
 package testCases;
 
-public class PostAdTC {
+import Pages.ChooseLocationPage;
+import Pages.PostAdPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PostAdTC extends  TestBase {
+    @Test
+    public void  ValidateOnErrorMessage() throws InterruptedException{
+        PostAdPage postAdPage =new PostAdPage(super.appiumDriver);
+          postAdPage.ClickOnCategoryField();
+        postAdPage.ChooseMainCategory();
+        postAdPage.ChooseSubCategory();
+        postAdPage.ClickOnDistrictField();
+        postAdPage.ClickOnDistrictLocationField();
+         ChooseLocationPage chooselocation= new ChooseLocationPage(super.appiumDriver);
+        chooselocation.ChooseDistrict();
+        chooselocation.ClickonDone();
+        chooselocation.ClickonAreaField();
+        chooselocation.ChooseArea();
+        chooselocation.ClickonDone();
+        chooselocation.ClickOnBlockField();
+        chooselocation.ChooseBlock();
+        chooselocation.ClickonDone();
+        chooselocation.ClickOnConfirmLocation();
+        postAdPage.EnterTitleField();
+        postAdPage.EnterPriceField();
+        postAdPage.ClickOnNextButton();
+    }
 }
--- src/test/java/testCases/ValidateOnMediaPageTC.java
+++ src/test/java/testCases/ValidateOnMediaPageTC.java
@@ -1,4 +1,17 @@
 package testCases;
 
+import Pages.AddMediaPageBase;
+import Pages.PostAdPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class ValidateOnMediaPageTC {
+    @Test
+    public void  ValidateOnErrorMessage() throws InterruptedException{
+       AddMediaPageBase addMediaPage = new AddMediaPageBase(super.appiumDriver);
+         pageTitle = addMediaPage.AsseronMediaPage();
+         Assert.assertEquals(pageTitle,"Add Media");
+    }
+
+
 }
